f61300b61a0bec243cb5d81cd1c62b31aaffb093
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-06-21 16:06:12 +0100

    Added getUserInfo and getWalletInfo to api.js

--- notification-client/src/api.js
+++ notification-client/src/api.js
@@ -18,3 +18,27 @@ export const sendFailedDepositNotification = async (
     throw new Error(error.response?.data || "Error sending notification");
   }
 };
+
+export const getUserInfo = async (userId) => {
+  try {
+    const response = await axios.get(
+      `${API_URL}/user-management/users/${userId}`
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data || "Error fetching user information");
+  }
+};
+
+export const getWalletInfo = async (userId) => {
+  try {
+    const response = await axios.get(
+      `${API_URL}/wallet-service/wallets/${userId}`
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response?.data || "Error fetching wallet information"
+    );
+  }
+};
