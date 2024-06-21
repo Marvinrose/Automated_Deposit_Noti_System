082cbd2024265916695ef5413c41a96036f58ed2
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-06-21 15:34:42 +0100

    Created api.js

--- notification-client/src/api.js
+++ notification-client/src/api.js
@@ -1,5 +1,20 @@
-const api = () => {
-  return <div></div>;
-};
+import axios from "axios";
 
-export default api;
+const API_URL = "http://localhost:3000";
+
+export const sendFailedDepositNotification = async (
+  userId,
+  amount,
+  notificationType
+) => {
+  try {
+    const response = await axios.post(`${API_URL}/notify-failed-deposit`, {
+      userId,
+      amount,
+      notificationType,
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data || "Error sending notification");
+  }
+};
