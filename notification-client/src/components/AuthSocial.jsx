2867152c36bbcf55a55d423c31434478a2b1a931
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-07-24 16:25:46 +0100

    Added sign in with google button to authsocial

--- notification-client/src/components/AuthSocial.jsx
+++ notification-client/src/components/AuthSocial.jsx
@@ -1,4 +1,4 @@
-import { Divider, IconButton, Stack } from "@mui/material";
+import { Button, Divider, IconButton, Stack } from "@mui/material";
 import { GithubLogo, GoogleLogo, TwitterLogo } from "phosphor-react";
 
 const AuthSocial = () => {
@@ -16,16 +16,35 @@ const AuthSocial = () => {
       >
         OR
       </Divider>
-      <Stack direction="row" justifyContent="center" spacing={2}>
-        <IconButton>
-          <GoogleLogo color="#DF3E30" />
-        </IconButton>
-        <IconButton color="inherit">
-          <GithubLogo />
-        </IconButton>
-        <IconButton>
-          <TwitterLogo color="#1C9CEA" />
-        </IconButton>
+      <Stack direction={"column"} spacing={2}>
+        <Button
+          fullWidth
+          color="inherit"
+          variant="contained"
+          type="submit"
+          size="large"
+          sx={{
+            bgcolor: "#000",
+            color: "#fff",
+            "&:hover": {
+              bgcolor: "#000",
+              color: "#fff",
+            },
+          }}
+        >
+          Sign In with Google
+        </Button>
+        <Stack direction="row" justifyContent="center" spacing={2}>
+          <IconButton>
+            <GoogleLogo color="#DF3E30" />
+          </IconButton>
+          <IconButton color="inherit">
+            <GithubLogo />
+          </IconButton>
+          <IconButton>
+            <TwitterLogo color="#1C9CEA" />
+          </IconButton>
+        </Stack>
       </Stack>
     </div>
   );
